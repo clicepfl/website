418b557a0a080396afe24f1ff4d49db233435192
Alexandre Chau <accounts@chau.moe>
2022-12-19 18:18:50 +0100

    chore: add devcontainer

--- strapi/config/database.ts
+++ strapi/config/database.ts
@@ -1,11 +1,29 @@
+import isDocker from 'is-docker';
 import path from 'path';
 
-export default ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+export default ({ env }) => {
+  const dbClient = env('DATABASE_CLIENT', 'sqlite')
+
+  const dbConnectionSettings = (dbClient === 'sqlite') ? ({
+    filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+  }) : ({
+    host: env('DATABASE_HOST'),
+    port: env('DATABASE_PORT'),
+    database: env('DATABASE_NAME'),
+    user: env('DATABASE_USER'),
+    password: env('DATABASE_PASSWORD'),
+  });
+
+  return {
     connection: {
-      filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
-});
+      client: dbClient,
+      connection: dbConnectionSettings,
+      useNullAsDefault: true,
+      pool: {
+        // When using Docker, change the pool min value to 0 as Docker will kill any idle connections
+        // https://docs.strapi.io/developer-docs/latest/setup-deployment-guides/configurations/required/databases.html#database-pooling-options
+        min: isDocker() ? 0 : undefined, // with undefined, strapi will grab its default value
+      }
+    }
+  }
+};
