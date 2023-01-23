3019176bacf503368885c4818606d7e1379bf681
Alexandre Chau <accounts@chau.moe>
2023-01-23 21:29:08 +0100

    refactor: add script args and error messages

--- strapi/.env-check.dev.ts
+++ strapi/.env-check.dev.ts
@@ -1,56 +1,148 @@
+/**
+ * This script checks and populates the local environment variables files
+ * SHOULD ONLY BE USED IN DEVELOPMENT ENVIRONMENTS!
+ */
 
-import Joi, { array } from "joi";
+import crypto from "crypto";
+import deepEqual from "deep-equal";
 import { parse, stringify } from "envfile";
 import fs from "fs";
-import { exit } from "process"
-import crypto from "crypto"
+import Joi, { Root as JoiInstance } from "joi";
+import { exit } from "process";
 
+// https://nodejs.org/api/process.html#process_exit_codes
+const FATAL_ERROR = 1;
+
+// Only run this script in development environments
 if (process.env.NODE_ENV !== "development") {
-    exit(1)
+  console.error(
+    "Error:",
+    "\n The env checker script should only be run in development environments!",
+    "\n Never run it in production!"
+  );
+  exit(FATAL_ERROR);
+}
+
+// Parses an env file and exits on error
+function readEnv(
+  filepath: string,
+  adviceOnError: string,
+  crashIfNotExists?: string
+): Record<string, string> {
+  if (!fs.existsSync(filepath)) {
+    if (crashIfNotExists) {
+      console.error(
+        `Error: the file ${filepath} does not exist`,
+        `\n ${crashIfNotExists}`
+      );
+      return exit(FATAL_ERROR);
+    } else {
+      return {};
+    }
+  }
+  try {
+    return parse(fs.readFileSync(filepath, "utf8"));
+  } catch (err) {
+    console.error(
+      `Error while reading the file ${filepath}: \n`,
+      err,
+      `\n ${adviceOnError}`
+    );
+    return exit(FATAL_ERROR);
+  }
 }
 
 // Adds default and failover values
 function defaultValue(schema, value) {
-    return schema.default(value)
+  return schema.default(value);
 }
 
-function randomBytes(): string {
-    return crypto.randomBytes(16).toString('base64')
+// Generates n random bytes of data
+function randomBytes(n: number): string {
+  return crypto.randomBytes(n).toString("base64");
 }
+
+// Generates a string of n comma-separated values
+function commaSeparated(n: number, factory: (() => any) | Array<() => any>) {
+  if (Array.isArray(factory)) {
+    if (factory.length !== n) {
+      console.error(
+        "Error:",
+        `\n Expected ${n} factory functions but got ${factory.length} instead`
+      );
+      exit(FATAL_ERROR);
+    }
+    return factory.map((f) => f());
+  } else {
+    return Array.from({ length: n }, () => factory());
+  }
+}
+
+// Extend Joi to validate comma-separated strings as arrays
+const joi = Joi.extend((joi) => ({
+  type: "stringArray",
+  base: joi.array(),
+  coerce: (value) => ({ value: value.split ? value.split(",") : value }),
+})) as JoiInstance & { stringArray(): Joi.ArraySchema };
 
 // .env file schema
-const schema = Joi.object({
-    HOST: defaultValue(Joi.string().ip({version: 'ipv4'}), '0.0.0.0'),
-    PORT: defaultValue(Joi.number().greater(1024), 1337),
-    APP_KEYS: defaultValue(Joi.string().base64(), [randomBytes(), randomBytes(), randomBytes(), randomBytes()].join(',')),
-    API_TOKEN_SALT: defaultValue(Joi.string().base64(), randomBytes()),
-    ADMIN_JWT_SECRET: defaultValue(Joi.string().base64(), randomBytes()),
-    JWT_SECRET: defaultValue(Joi.string().base64(), randomBytes())
-}).unknown(true);
-
-
+const schema = joi
+  .object({
+    HOST: defaultValue(joi.string().ip({ version: "ipv4" }), "0.0.0.0"),
+    PORT: defaultValue(joi.number().greater(1024), 1337),
+    APP_KEYS: defaultValue(
+      joi.stringArray().items(joi.string().base64()).length(4),
+      commaSeparated(4, () => randomBytes(16))
+    ),
+    API_TOKEN_SALT: defaultValue(joi.string().base64(), randomBytes(16)),
+    ADMIN_JWT_SECRET: defaultValue(joi.string().base64(), randomBytes(16)),
+    JWT_SECRET: defaultValue(joi.string().base64(), randomBytes(16)),
+  })
+  .unknown(true);
 
 // Reads .env
-let env = {}
-if (fs.existsSync('.env')) {
-    try {
-        const data = fs.readFileSync('.env', 'utf8');
-        env = parse(data);
-    } catch (err) {
-        console.error(err);
-        exit(-2) // TBD: set env to {} and continue ?
-    }
-} else {
-    env = {}
+const env = readEnv(
+  ".env",
+  "If you do not have local changes in your .env file, try removing it and running this script again"
+);
+
+// Ensures that .env.example is up-to-date with .env (the opposite may not be true yet)
+if (process.argv.includes("--with-example")) {
+  // Reads .env.example
+  const example = readEnv(
+    ".env.example",
+    "Make sure that you did not modify the .env.example file, and compare it to the file on the remote repository",
+    "Make sure that you did not remove the .env.example file, and check that the file exists on the remote repository"
+  );
+
+  if (!Object.keys(env).every((key) => example.hasOwnProperty(key))) {
+    console.error(
+      "Error: some environment variables defined in .env do not have a corresponding entry in .env.example"
+    );
+    exit(FATAL_ERROR);
+  }
 }
 
 // Completes schema
-const validation = schema.validate(env)
-if (validation.error){
-    console.error(validation.error.message)
-    exit(-1)
+const validation = schema.validate(env);
+if (validation.error) {
+  console.error(
+    "Validation of .env file was unsucessful: \n ",
+    validation.error.message,
+    "\n If you do not have local changes in your .env file, try removing it and running this script again"
+  );
+  exit(FATAL_ERROR);
 }
 
-
-// Write to .env
-fs.writeFileSync('.env', stringify(validation.value))
+// Writes to .env
+const original = schema.validate(env, { noDefaults: true });
+if (
+  process.argv.includes("--write") &&
+  !deepEqual(original.value, validation.value, { strict: true })
+) {
+  fs.writeFileSync(".env", stringify(validation.value));
+  console.log(
+    "The following configuration was written to .env:",
+    validation.value
+  );
+}
