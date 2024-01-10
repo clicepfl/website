21544323c7e299251ec1fc84a40e495fc688916c
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-01-10 15:42:59 +0100

    feat: improve background management

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import Background from "@/assets/background.svg";
+import Background from "../../public/background.svg?url";
 import NavigationBar from "@/components/NavigationBar";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
@@ -7,11 +7,11 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <NavigationBar />
-      <div className="main">
+      <div
+        className="main"
+        style={{ backgroundImage: `url(${Background.src})` }}
+      >
         <Component {...pageProps} />
-      </div>
-      <div className="background">
-        <Background />
       </div>
     </>
   );
