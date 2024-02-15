ce9ab860694c9e2192306f397874862bb88c6d47
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-02-15 06:13:36 +0100

    fix: schema.d.ts generation

--- save-directus.sh
+++ save-directus.sh
@@ -3,5 +3,6 @@
 echo "[INFO] Creating data model snapshot"
 docker exec -it clic-directus npx directus schema snapshot /share/snapshot.yaml -y
 echo "[INFO] Generating typescript declarations"
-docker exec -it clic-directus npx directus-typescript-gen --email clic@epfl.ch --password 1234 -h http://localhost:8055 -y > app/src/types/schema.d.ts
+docker exec -it clic-directus npx directus-typescript-gen --email clic@epfl.ch --password 1234 -h http://localhost:8055 -o /share/schema.d.ts
+mv directus/schema.d.ts app/src/types/schema.d.ts
 npx prettier --write app/src/types/schema.d.ts directus/snapshot.yaml
