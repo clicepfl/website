bd92c6fc1a382f20c8c28bfd58f283b99a27b421
Ludovic Mermod <ludovic.mermod@gmail.ch>
2024-02-19 21:51:28 +0100

    feat: add type aliases for data model

---
+++ app/src/types/aliases.ts
@@ -0,0 +1,10 @@
+import { Schema } from "./schema";
+
+export type Association = Schema["association"][0];
+export type AssociationMembership = Schema["association_memberships"][0];
+export type Commission = Schema["commissions"][0];
+export type CommissionMembership = Schema["commission_memberships"][0];
+export type Member = Schema["members"][0];
+export type News = Schema["news"][0];
+export type Partner = Schema["partners"][0];
+export type SocialLink = Schema["social_links"][0];
