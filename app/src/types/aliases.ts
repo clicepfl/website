9137ebbab63220181261af146e7701af7d3ff507
Ludovic Mermod <ludovic.mermod@gmail.ch>
2024-02-19 23:15:21 +0100

    fix: hardfix for singleton type generation

--- app/src/types/aliases.ts
+++ app/src/types/aliases.ts
@@ -1,6 +1,6 @@
 import { Schema } from "./schema";
 
-export type Association = Schema["association"][0];
+export type Association = Schema["association"];
 export type AssociationMembership = Schema["association_memberships"][0];
 export type Commission = Schema["commissions"][0];
 export type CommissionMembership = Schema["commission_memberships"][0];
