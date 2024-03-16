7f2e82f19e842607e20291ab49c999719067d338
sidonie <sidonie.bouthors@epfl.ch>
2024-03-16 13:15:38 +0100

    feat: add button component

---
+++ app/src/components/Button.tsx
@@ -0,0 +1,22 @@
+import styles from "@/styles/Button.module.scss";
+import Link from "next/link";
+
+export default function Card(
+  props: {
+    text: string;
+    link: string;
+    linkTarget?: "_blank";
+  } & { size: "small" | "large" }
+) {
+  let text = props.text;
+  let link = props.link;
+  let linkTarget = props.linkTarget;
+
+  return (
+    <>
+      <Link href={link} target={linkTarget}>
+        <div className={`${styles.button} ${styles[props.size]}`}>{text}</div>
+      </Link>
+    </>
+  );
+}
