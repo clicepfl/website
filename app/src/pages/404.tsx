540ec9af06db98a9dcacc69cfb8e7a1253003545
Noé Terrier <noeterrier@proton.me>
2024-04-09 13:44:51 +0200

    style: 404 styling

--- app/src/pages/404.tsx
+++ app/src/pages/404.tsx
@@ -1,18 +1,22 @@
+import Background from "@/assets/background.svg";
 import Button from "@/components/Button";
 import TabTitle from "@/components/TabTitle";
-import styles from "@/styles/Page.module.scss";
+import styles from "@/styles/404.module.scss";
 import { useRouter } from "next/router";
 
 export default function NotFound() {
   const router = useRouter();
   return (
     <>
+      <Background className={styles.background} name="background" />
       <TabTitle title="Not found" />
 
       <div className={styles.main}>
-        <h1>{"Error 404"}</h1>
-        <h4>{"You are lost :/"}</h4>
-        <Button text="Home" onClick={() => router.push("/")} />
+        <div className={styles.center}>
+          <h1>{"Error 404"}</h1>
+          <h4>{"You are lost :/"}</h4>
+          <Button text="Home" onClick={() => router.push("/")} />
+        </div>
       </div>
     </>
   );
