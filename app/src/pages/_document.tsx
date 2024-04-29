43843e52780f6e1ea9740579d0d2a7991042aa4f
Noé Terrier <noeterrier@proton.me>
2024-04-29 11:37:41 +0200

    fix: favicon paths

--- app/src/pages/_document.tsx
+++ app/src/pages/_document.tsx
@@ -7,21 +7,21 @@ export default function Document() {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/dev/apple-touch-icon.png"
+          href="/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/dev/favicon-32x32.png"
+          href="/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/dev/favicon-16x16.png"
+          href="/favicon-16x16.png"
         />
-        <link rel="manifest" href="/dev/site.webmanifest" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <body>
         <Main />
