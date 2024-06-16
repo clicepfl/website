ebd290284c847e5050dce8aec09930618f363b1c
Noé Terrier <noeterrier@proton.me>
2024-06-16 16:44:43 -0700

    feat: add email to pole's description component (#60)
    Co-authored-by: Ludovic Mermod <ludovic.mermod@epfl.ch>

--- app/src/components/PoleDescription.tsx
+++ app/src/components/PoleDescription.tsx
@@ -1,5 +1,5 @@
 import MembersList from "./MembersList";
-import { getTranslation, locale } from "@/locales";
+import { getTranslation, locale, useTranslationTable } from "@/locales";
 import markdownStyle from "@/styles/Markdown.module.scss";
 import styles from "@/styles/Page.module.scss";
 import {
@@ -16,6 +16,7 @@ export default function PoleDescription(props: {
 }) {
   const router = useRouter();
   const translation = getTranslation(props.pole, locale(router));
+  const tt = useTranslationTable();
 
   return (
     <div className={styles.section}>
@@ -28,6 +29,16 @@ export default function PoleDescription(props: {
         </div>
       </div>
       <MembersList title={false} membership={props.members} />
+      {props.pole.mail ? (
+        <div className={styles.center}>
+          <div className={styles.info}>
+            <p>{tt["contact"]}</p>
+            <a href={"mailto:" + props.pole.mail}>{props.pole.mail}</a>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
