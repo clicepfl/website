46ea62cd8490c04a3e2e7c531a39793fb54178e5
Noé Terrier <noeterrier@proton.me>
2025-01-08 15:00:14 +0100

    feat: icbd page skeleton (#90)
    Co-authored-by: Ludovic Mermod <ludovic.mermod@epfl.ch>

---
+++ src/components/Particles.tsx
@@ -0,0 +1,96 @@
+import { ISourceOptions } from "@tsparticles/engine";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+import { useEffect, useMemo, useState } from "react";
+
+export default function ParticlesComponent(props: { id: string }) {
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const options: ISourceOptions = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "#ffffff",
+        },
+      },
+      fullScreen: {
+        zIndex: -1,
+        enable: true, // this is the line to change
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "repulse",
+          },
+          onHover: {
+            enable: true,
+            mode: "grab",
+          },
+        },
+        modes: {
+          push: {
+            distance: 200,
+            duration: 15,
+          },
+          grab: {
+            distance: 150,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#FE9F21",
+        },
+        links: {
+          color: "#FE9F21",
+          distance: 150,
+          enable: true,
+          opacity: 0.3,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          random: true,
+          speed: 3,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 200,
+        },
+        opacity: {
+          value: 7.0,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 4 },
+        },
+      },
+      detectRetina: true,
+    }),
+    []
+  );
+
+  if (init) {
+    return <Particles id={props.id} options={options} />;
+  }
+
+  return <></>;
+}
---
+++ src/pages/icbd.tsx
@@ -0,0 +1,243 @@
+import Card from "@/components/Card";
+import DirectusImage from "@/components/DirectusImage";
+import IcbdActivityCard from "@/components/IcbdActivityCard";
+import ParticlesComponent from "@/components/Particles";
+import TabTitle from "@/components/TabTitle";
+import { directus, getDirectusImageUrl, populateLayoutProps } from "@/directus";
+import { getTranslation } from "@/locales";
+import style from "@/styles/ICBDPage.module.scss";
+import pageStyle from "@/styles/Page.module.scss";
+import { ICBD, ICBDActivity, ICBDPhd, ICBDSpeaker } from "@/types/aliases";
+import { readItems, readSingleton } from "@directus/sdk";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { useRouter } from "next/router";
+import Markdown from "react-markdown";
+
+export default function ICBDPage(
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) {
+  const router = useRouter();
+  const translation = getTranslation(props.icbd, router.locale);
+
+  const formattedDate = new Intl.DateTimeFormat(router.locale, {
+    weekday: "long", // full weekday name (e.g., 'Monday')
+    year: "numeric", // full year (e.g., 2025)
+    month: "long", // full month name (e.g., 'January')
+    day: "numeric", // day of the month (e.g., 5)
+  }).format(new Date(props.icbd.date || ""));
+
+  const formatTime = (time: string): string => {
+    const [hours, minutes] = time.split(":").map(Number); // Split and parse hours and minutes
+    const period = hours >= 12 ? "pm" : "am"; // Determine AM/PM
+    const formattedHour = hours % 12 || 12; // Convert 24-hour time to 12-hour time, handling midnight (0 becomes 12)
+    return `${formattedHour}${minutes ? `:${minutes}` : ""}${period}`;
+  };
+
+  const formattedSecondTitle = `${formatTime(
+    props.icbd.start_time || ""
+  )} to ${formatTime(props.icbd.end_time || "")}, ${props.icbd.place}`;
+
+  return (
+    <>
+      <style jsx global>{`
+        body {
+          background: white;
+        }
+      `}</style>
+
+      <TabTitle title={"ICBD"} image={getDirectusImageUrl(props.icbd.logo)} />
+
+      <div className={style.header}>
+        <div className={style.logo_div}>
+          <DirectusImage
+            sizes={"50rem"}
+            name={"icbd"}
+            img={props.icbd.logo}
+            className={style.logo}
+          />
+          <h1>{formattedDate}</h1>
+          <h2>{formattedSecondTitle}</h2>
+        </div>
+
+        <ParticlesComponent id={style.particles} />
+      </div>
+
+      <div className={style.mainDiv}>
+        <div className={pageStyle.main}>
+          <div className={pageStyle.center}>
+            <div className={style.registration}>
+              <h1>Registration</h1>
+              <Markdown>{translation.registration_instructions}</Markdown>
+              {/* <div className={style.buttons}>
+                <script
+                  id="luma-checkout"
+                  src="https://embed.lu.ma/checkout-button.js"
+                />
+                <a
+                  href="https://lu.ma/event/evt-jl8ySRI9GGAtkWm"
+                  className="luma-checkout--button"
+                  data-luma-action="checkout"
+                  data-luma-event-id="evt-jl8ySRI9GGAtkWm"
+                >
+                  Talks and meetups
+                </a>
+                <a
+                  href="https://lu.ma/event/evt-sddFjg2Xixyj5PV"
+                  className="luma-checkout--button"
+                  data-luma-action="checkout"
+                  data-luma-event-id="evt-sddFjg2Xixyj5PV"
+                >
+                  Mock interviews
+                </a>
+                <a
+                  href="https://lu.ma/event/evt-yzNqzZ2llO3kzoL"
+                  className="luma-checkout--button"
+                  data-luma-action="checkout"
+                  data-luma-event-id="evt-yzNqzZ2llO3kzoL"
+                >
+                  Speed Networking
+                </a>
+              </div> */}
+            </div>
+
+            <Markdown>{translation.description}</Markdown>
+            {props.icbd.presentation_video ? (
+              <video
+                width="600"
+                controls
+                preload="none"
+                className={pageStyle.video}
+              >
+                <source src={props.icbd.presentation_video} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            ) : (
+              <></>
+            )}
+          </div>
+        </div>
+
+        <div className={style.partners}>
+          <div className={style.partnersList}>
+            {props.icbd.partners_images?.map((file) =>
+              typeof file === "number" ? (
+                <></>
+              ) : (
+                <DirectusImage
+                  sizes="20rem"
+                  img={file.directus_files_id}
+                  key={
+                    typeof file.directus_files_id === "string"
+                      ? file.directus_files_id
+                      : file.directus_files_id?.id
+                  }
+                  className={style.partnerImage}
+                />
+              )
+            )}
+          </div>
+        </div>
+
+        <div className={pageStyle.main}>
+          <h1>Alumni</h1>
+          <h2>speakers present at the event</h2>
+
+          <div className={style.alumni}>
+            <div className={style.alumniList}>
+              {props.speakers.map((speaker: ICBDSpeaker) => (
+                <Card
+                  key={speaker.id}
+                  img={speaker.picture}
+                  title={`${speaker.first_name} ${speaker.last_name}` || ""}
+                  description={speaker.company || ""}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className={pageStyle.main}>
+          <h1>PHD students</h1>
+          <h2>during the poster session</h2>
+          <div className={style.alumni}>
+            <div className={style.alumniList}>
+              {props.phds.map((phd: ICBDPhd) => (
+                <Card
+                  key={phd.id}
+                  img={phd.picture}
+                  title={`${phd.first_name} ${phd.last_name}` || ""}
+                  description={phd.laboratory || ""}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className={pageStyle.main}>
+          <h1>Timetable</h1>
+          <DirectusImage
+            img={props.icbd.timetable}
+            name="timetable"
+            sizes="50rem"
+            className={style.timetable}
+          />
+        </div>
+
+        <div className={style.activities}>
+          <h1>Activities</h1>
+
+          <div className={style.activitiesList}>
+            {props.activities.map((activity: ICBDActivity) => (
+              <IcbdActivityCard key={activity.id} activity={activity} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<{
+  icbd: ICBD;
+  speakers: ICBDSpeaker[];
+  phds: ICBDPhd[];
+  activities: ICBDActivity[];
+}> = populateLayoutProps(async (_) => {
+  return {
+    props: {
+      icbd: await directus().request(
+        // @ts-ignore
+        readSingleton("ICBD", {
+          fields: [
+            "logo",
+            "presentation_video",
+            "date",
+            "place",
+            "start_time",
+            "end_time",
+            "timetable",
+            { partners_images: ["*"] },
+            {
+              translations: ["*"],
+            },
+          ],
+        })
+      ),
+      speakers: await directus().request(
+        readItems("icbd_speakers", {
+          fields: ["picture", "first_name", "last_name", "company"],
+        })
+      ),
+      phds: await directus().request(
+        readItems("icbd_phds", {
+          fields: ["picture", "first_name", "last_name", "laboratory"],
+        })
+      ),
+      activities: await directus().request(
+        readItems("icbd_activities", {
+          fields: ["hosts", "icon", { translations: ["*"] }],
+        })
+      ),
+    },
+  };
+});
